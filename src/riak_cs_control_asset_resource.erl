36a3e4c38458c99245d0a790629880c5717cea1d
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-12 12:08:17 -0500

    Fix bothersome whitespace.

--- src/riak_cs_control_asset_resource.erl
+++ src/riak_cs_control_asset_resource.erl
@@ -6,8 +6,8 @@
 -module(riak_cs_control_asset_resource).
 -author('Christopher Meiklejohn <christopher.meiklejohn@gmail.com>').
 
--export([init/1, 
-         allowed_methods/2, 
+-export([init/1,
+         allowed_methods/2,
          content_types_provided/2,
          resource_exists/2,
          to_resource/2]).
@@ -16,32 +16,32 @@
 
 init([]) -> {ok, undefined}.
 
-allowed_methods(ReqData, Context) -> 
+allowed_methods(ReqData, Context) ->
     {['HEAD', 'GET'], ReqData, Context}.
 
-content_types_provided(ReqData, Context) -> 
+content_types_provided(ReqData, Context) ->
     Path = get_path(ReqData),
     MimeType = webmachine_util:guess_mime(Path),
     {[{MimeType, to_resource}], ReqData, Context}.
 
-resource_exists(ReqData, Context) -> 
+resource_exists(ReqData, Context) ->
     Path = get_path(ReqData),
-    case filelib:is_regular(Path) of 
-        true -> 
+    case filelib:is_regular(Path) of
+        true ->
             {true, ReqData, Context};
-        _ -> 
+        _ ->
             {false, ReqData, Context}
     end.
 
-to_resource(ReqData, Context) -> 
+to_resource(ReqData, Context) ->
     Path = get_path(ReqData),
     {ok, Source} = file:read_file(Path),
     {Source, ReqData, Context}.
 
-get_path(ReqData) -> 
+get_path(ReqData) ->
     {ok, App} = application:get_application(?MODULE),
     Filename = case wrq:disp_path(ReqData) of
-        "" -> 
+        "" ->
             ["index.html"];
         _ ->
             wrq:path_tokens(ReqData)
