20bed46c159360b75b2059f4c5ab8359bb42f8af
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-19 14:43:16 -0500

    Add basic user listing.

--- priv/www/js/router.js
+++ priv/www/js/router.js
@@ -7,7 +7,7 @@ minispade.register('router', function() {
         redirectsTo: 'users.index'
       }),
 
-      viewUser: Ember.Route.transitionTo('users.show'),
+      viewUsers: Ember.Route.transitionTo('users.index'),
 
       users: Ember.Route.extend({
         route: '/users',
@@ -18,35 +18,6 @@ minispade.register('router', function() {
 
         index: Ember.Route.extend({
           route: '/'
-        }),
-
-        show: Ember.Route.extend({
-          route: '/:key_id',
-
-          // Nasty hack; when loading the object directly when it's not
-          // in memory, key_id isn't available yet, even though it's the
-          // primary key.  Use the _id property when we need to load on
-          // demand, and key_id if it's available.
-          serialize: function(router, context) {
-            var key_id = context.get('key_id') || context.get('_id');
-
-            return {
-              key_id: key_id
-            };
-          },
-
-          deserialize: function(router, params) {
-            return RiakCsControl.User.find(params.key_id);
-          },
-
-          connectOutlets: function(router, context) {
-            router.get('usersController').connectOutlet('user', context);
-            router.get('usersController').set('selectedUser', context);
-          },
-
-          exit: function(router) {
-            router.get('usersController').set('selectedUser', undefined);
-          }
         })
       })
     })
