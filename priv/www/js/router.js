0a6e6decf822ac5a1bdb94d37ca5c04f0b27ed7d
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-02-06 15:08:31 -0500

    Further router updates.

--- priv/www/js/router.js
+++ priv/www/js/router.js
@@ -12,12 +12,6 @@ minispade.register('router', function() {
     }
   });
 
-  RiakCsControl.UsersRoute = Ember.Route.extend({
-    renderTemplate: function() {
-      this.render('users');
-    }
-  });
-
   RiakCsControl.UsersIndexRoute = Ember.Route.extend({
     model: function() {
       return RiakCsControl.User.find();
@@ -25,21 +19,6 @@ minispade.register('router', function() {
 
     setupController: function(controller, model) {
       controller.set('content', model);
-    },
-
-    renderTemplate: function() {
-      this.render('users_index');
-    }
-  });
-
-  RiakCsControl.UsersNewRoute = Ember.Route.extend({
-    enter: function() {
-      var controller = this.get('controller');
-      console.log(controller);
-    },
-
-    renderTemplate: function() {
-      this.render('users_new');
     }
   });
 
