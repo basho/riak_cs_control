c735bb2b1dd9959f25243d9c897e2c1bce72e78d
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-09-05 14:47:53 -0400

    Adapt d3 PieChart code from Riak Control (wip)

--- priv/www/js/controllers.js
+++ priv/www/js/controllers.js
@@ -27,6 +27,144 @@ minispade.register('controllers', function() {
     }
   });
 
+  /**
+   * @class
+   *
+   * Pie chart mixin.
+   */
+  RiakCsControl.PieChart = Ember.Mixin.create(
+    /** @scope RiakCsControl.PieChart.prototype */ {
+
+    /**
+     * Pie chart dimensions.
+     */
+    width: 120,
+    height: 120,
+
+    /**
+     * Radius of the pie chart.
+     *
+     * @returns {number}
+     */
+    radius: function() {
+      var width = this.get('width');
+      var height = this.get('height');
+
+      return Math.min(width, height) / 2;
+    }.property('width', 'height'),
+
+    /**
+     * Arc rendering function, computed from outer and inner radius.
+     *
+     * @returns {function}
+     */
+    arc: function() {
+      var radius = this.get('radius');
+
+      return d3.svg.arc().innerRadius(radius - 20).
+                          outerRadius(radius - 9);
+    }.property('radius'),
+
+    /**
+     * Generate an svg, and insert into the DOM.
+     *
+     * @returns {function}
+     */
+    svg: function() {
+      var id = this.get('id');
+      var width = this.get('width');
+      var height = this.get('height');
+
+      return d3.select(id).
+        append("svg").
+          attr("width", width).
+          attr("height", height).
+        append("g").
+          attr("transform",
+               "translate(" + width / 2 + "," + height / 2 + ")");
+    }.property('width', 'height', 'id'),
+
+    /**
+     * Observer which redraws the path components into the svg element 
+     * as the data changes, while also triggering the motion tween.
+     *
+     * @returns {true}
+     */
+    path: function() {
+      var svg =      this.get('svg');
+      var arc =      this.get('arc');
+      var pie =      this.get('pie');
+      var data =     this.get('data');
+      var arcTween = this.get('arcTween');
+
+      var normalColor =   this.get('normalColor');
+      var abnormalColor = this.get('abnormalColor');
+
+      var path = svg.selectAll("path").data(pie(data));
+
+      path.enter().append("path");
+
+      path.attr("fill", function(d, i) {
+                  return i === 0 ? abnormalColor : normalColor; }).
+          attr("d", arc).
+          style("stroke", "rgba(0, 0, 0, .7)").
+          style("stroke-width", "2px").
+          each(function(d) { this._current = d; });
+
+      path.transition().duration(750).attrTween("d", arcTween);
+
+      return true;
+    }.observes('data'),
+
+    /**
+     * Tween interpolation function for arcs.
+     *
+     * @returns {function}
+     */
+    arcTween: function() {
+      var arc = this.get('arc');
+
+      return function(a) {
+        var i = d3.interpolate(this._current, a);
+        this._current = i(0);
+        return function(t) {
+          return arc(i(t));
+        };
+      };
+    }.property('arc'),
+
+    /**
+     * Generate a pie chart layout.
+     *
+     * @returns {function}
+     */
+    pie: function() {
+      return d3.layout.pie().sort(null);
+    }.property(),
+
+    /**
+     * Whenever the view is reinserted into the DOM, re-render
+     * the path components into the view.
+     *
+     * @returns {void}
+     */
+    didInsertElement: function() {
+      // Force rendering when the view is reinserted into the DOM.
+      this.path();
+    },
+
+    /**
+     * Return normalized abnormal amount.
+     *
+     * @returns {number}
+     *
+     */
+    normalizedAbnormal: function() {
+      return this.get('data')[0];
+    }.property('data'),
+
+  });
+
   RiakCsControl.UsersIndexController = Ember.ArrayController.extend({
     sortProperties: ['isNormal', 'email'],
 
@@ -67,6 +205,49 @@ minispade.register('controllers', function() {
       update.call(user);
       transaction.commit();
     }
+  });
+
+  /**
+   * @class
+   *
+   * Container view for the disk usage chart.
+   */
+  RiakCsControl.DiskUsageChart = Ember.View.extend(
+    RiakCsControl.PieChart,
+    /** @scope RiakCsControl.DiskUsageChart.prototype */ {
+    templateName: 'disk-usage-chart',
+
+    classNames: ['chart'],
+
+    data: function() {
+      // var partitionCount = this.get('partitionCount');
+      // var quorumUnavailableCount = this.get('quorumUnavailableCount');
+
+      var clusterDiskUsageKb = 0;
+      var clusterDiskFreeKb = 0;
+
+      var normalizedDiskFree;
+      var normalizedDiskUsed;
+
+      if(false) {
+        // normalizedAbnormal =
+        //   Math.round((quorumUnavailableCount / partitionCount) * 100);
+        // normalizedPartitions = 100 - normalizedAbnormal;
+      } else {
+        // Default
+        normalizedDiskFree = 100;
+        normalizedDiskUsed = 0;
+      }
+
+      return [normalizedDiskUsed, normalizedDiskFree];
+    }.property('clusterDiskUsageKb', 'clusterDiskFreeKb'),
+
+    id: '#disk-usage-chart',
+
+    abnormalColor: "#ffb765",
+
+    normalColor: "#84ff7e"
+
   });
 
   RiakCsControl.UsersNewController = Ember.ObjectController.extend({
